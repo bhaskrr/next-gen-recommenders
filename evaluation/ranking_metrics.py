f82fb77d04ce7b5bcc2e69997245ce0d6d7eee76
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2026-02-22 23:03:12 +0530

    added hit rate@k

--- evaluation/ranking_metrics.py
+++ evaluation/ranking_metrics.py
@@ -32,3 +32,16 @@ def recall_at_k(recommended, relevant, k):
     recommended_k = recommended[:k]
     hits = len(set(recommended_k) & relevant)
     return hits / len(relevant)
+
+# A hit occurs when an item recommended to a user in their Top-list is
+# actually consumed, clicked, or highly rated by that user in the test set.
+# In our case, if any of the recommended items in top k are relevant to the user, we will consider it a hit
+# with a hit rate of 100% and otherwise 0%
+def hit_rate_at_k(recommended, relevant, k):
+    """
+    Returns 1 if at least one relevant item appears in top-k
+    """
+    
+    recommended_k = recommended[:k]
+    hits = len(set(recommended_k) & relevant)
+    return 1.0 if len(hits) > 0 else 0.0
