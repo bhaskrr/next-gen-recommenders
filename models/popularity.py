fd3a6d14c3430aef27c3ae03720f10c17f3073ff
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2026-03-01 23:22:45 +0530

    added recommend method to popularity recommender

--- models/popularity.py
+++ models/popularity.py
@@ -13,3 +13,21 @@ class PopularityRecommender:
         # Sort items by descending popularity
         self.item_popularity = item_counts.sort_values(ascending=False)
         self.ranked_items = list(self.item_popularity.index)
+
+    def recommend(self, user_id, user_train_items, k= 10):
+        """
+        Recommend top-k unseen popular items for a user.
+        
+        user_train_items: dict {user_id: set(items)}
+        """
+        seen_items = user_train_items.get(user_id, set())
+        
+        recommendations = []
+        
+        for item in self.ranked_items:
+            if item not in seen_items:
+                recommendations.append(item)
+            if len(recommendations) >= k:
+                break
+                
+        return recommendations
