481a91d7a5875ce95e99cac44b8e7c6f829af947
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2026-03-01 22:47:15 +0530

    added fit method to popularity recommender

--- models/popularity.py
+++ models/popularity.py
@@ -1,6 +1,15 @@
-
+import pandas as pd
 
 class PopularityRecommender:
     def __init__(self):
         self.item_popularity = None
         self.ranked_items = None
+    
+    def fit(self, train_df: pd.DataFrame):
+        """Compute item popularity from train data."""
+        
+        item_counts = train_df.groupby("movieId").size()
+        
+        # Sort items by descending popularity
+        self.item_popularity = item_counts.sort_values(ascending=False)
+        self.ranked_items = list(self.item_popularity.index)
